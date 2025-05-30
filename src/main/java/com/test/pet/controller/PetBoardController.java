7547461d55fbac8cc398068be41d72eb77d7f0ff
zajinmori <wnahd0310@naver.com>
2025-05-30 17:58:35 +0900

    feat: 로그아웃 기능 구현

--- src/main/java/com/test/pet/controller/PetBoardController.java
+++ src/main/java/com/test/pet/controller/PetBoardController.java
@@ -2,14 +2,16 @@ package com.test.pet.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class PetBoardController {
 
 	//유기동물 목록 컨트롤러
 	@GetMapping("/petboard.do")
-	public String petboard() {
+	public String petBoard() {
 		
 		return "board/petboard";
 	}
+
 }
